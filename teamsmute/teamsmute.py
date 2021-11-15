ce8800ab76e2dd1f11ad51f4328ebab04dfee260
AzureAD\BruceVaughan <bruce.vaughan@iotnxt.com>
2021-11-15 09:09:18 +0200

    Update teamsmute

--- teamsmute/teamsmute.py
+++ teamsmute/teamsmute.py
@@ -4,6 +4,34 @@ from flask import Flask
 from flask_restful import Resource, Api
 from dotenv import load_dotenv
 import os
+import win32gui
+import re
+
+class WindowMgr:
+    """Encapsulates some calls to the winapi for window management"""
+
+    def __init__ (self):
+        """Constructor"""
+        self._handle = None
+
+    def find_window(self, class_name, window_name=None):
+        """find a window by its class_name"""
+        self._handle = win32gui.FindWindow(class_name, window_name)
+
+    def _window_enum_callback(self, hwnd, wildcard):
+        """Pass to win32gui.EnumWindows() to check all the opened windows"""
+        if re.match(wildcard, str(win32gui.GetWindowText(hwnd))) is not None:
+            self._handle = hwnd
+
+    def find_window_wildcard(self, wildcard):
+        """find a window whose title matches the wildcard regex"""
+        self._handle = None
+        win32gui.EnumWindows(self._window_enum_callback, wildcard)
+
+    def set_foreground(self):
+        """put the window in the foreground"""
+        win32gui.SetForegroundWindow(self._handle)
+
 
 class Teams(Resource):
     def post(self):
@@ -11,14 +39,20 @@ class Teams(Resource):
     
 
 def sendMuteKeys():
-    print("Mute/Unmute teams.")
-    keyboard = Controller()
-    keyboard.press(Key.ctrl)
-    keyboard.press(Key.shift)
-    keyboard.press("m")
-    keyboard.release("m")
-    keyboard.release(Key.shift)
-    keyboard.release(Key.ctrl)
+    w = WindowMgr()
+    a = w.find_window_wildcard(".*Meeting .*")
+    if w._handle == None:
+        print("Teams Meeting is not running")
+    else:
+        print("Mute/Unmute teams.")
+        w.set_foreground()
+        keyboard = Controller()
+        keyboard.press(Key.ctrl)
+        keyboard.press(Key.shift)
+        keyboard.press("m")
+        keyboard.release("m")
+        keyboard.release(Key.shift)
+        keyboard.release(Key.ctrl)
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')  # Path to .env file
 load_dotenv(dotenv_path)
